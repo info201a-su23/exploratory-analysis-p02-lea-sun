a80e893bd184c5274ab401467286ec749b0a8470
Jullie24 <139392709+Jullie24@users.noreply.github.com>
2023-08-04 12:18:05 -0700

    initial commit

--- chart2_example.R
+++ chart2_example.R
@@ -1,1 +1,10 @@
+library(ggplot2)
 
+x_values <- c('Children (3⁠–17 years)', 'Adults (18+ years)', 'Males (18+ years)', 'Females (18+ years)')
+y_values <- c(3.2, 7.1, 5.3, 8.7)
+
+ggplot() +
+  geom_bar(aes(x = x_values, y = y_values), stat = 'identity', fill = 'blue') +
+  labs(x = "Population", y = "Prevalence of Depression (%)") +
+  ggtitle("Prevalence of Depression in Different Populations") +
+  theme_minimal()
