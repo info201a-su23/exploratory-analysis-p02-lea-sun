2d7a399d93238a86d5d256c4d022311600542a9c
Jullie24 <139392709+Jullie24@users.noreply.github.com>
2023-08-04 19:41:54 -0700

    Initial commit

--- chart2_example.R
+++ chart2_example.R
@@ -1,14 +1,13 @@
 library(ggplot2)
 
-conditions <- c("Major Depression", "Bipolar Disorder", "Eating Disorder", "Anxiety Disorders")
+disorders <- c("Schizophrenia", "Depressive Disorder", "Anxiety Disorder", "Bipolar Disorder", "Eating Disorders")
+prevalence <- c(0.3, 3.4, 3.8, 0.5, 0.2)
 
-prevalence <- c(35.40, 12.90, 15.20, 44.20)
+data <- data.frame(Disorder = disorders, Prevalence = prevalence)
 
-data <- data.frame(Condition = conditions, Prevalence = prevalence)
-
-ggplot(data, aes(x = Condition, y = Prevalence, fill = Condition)) +
-  geom_bar(stat = "identity", position = "dodge") +
-  labs(x = "Mental Health Conditions", y = "Prevalence (%)") +
-  ggtitle("Prevalence of Mental Health Conditions Worldwide") +
+ggplot(data, aes(x = Disorder, y = Prevalence, fill = Disorder)) +
+  geom_bar(stat = "identity") +
+  labs(x = "Mental Health Disorders", y = "Prevalence (%)") +
+  ggtitle("Prevalence of Mental Health Disorders Worldwide") +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
