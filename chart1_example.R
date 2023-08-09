a9a0936e9aaa15299bdee82499f96a4ff43bca64
ksimran004 <sk04@uw.edu>
2023-08-09 15:17:33 -0700

    chart 1 updates

--- chart1_example.R
+++ chart1_example.R
@@ -1,7 +1,21 @@
-x_values <- seq(1, 3)
-y_values <- seq(1,3)
+library(ggplot)
+library(dplyr)
 
-library(ggplot2)
-ggplot() +
-  geom_point(aes(x=x_values, y = y_values))
+Country <- c("United States", "Afghanistan")
+Prevalence <- c(3.098053, 4.424521)
 
+data <- data.frame(Country = Country, Prevalence = Prevalence)
+
+chart1 <- ggplot2(data, aes(x = Country, y = Prevalence, fill = Country, colour = Country )) +
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(x = "Country", y = "Prevalence of Depression (%)") +
+  ggtitle("Comparison of Depression of the two Countries by age group 20-24 years") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+print(chart1)
+
+
+
+  
+  
