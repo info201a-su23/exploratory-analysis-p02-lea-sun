fb448c1dab2a88b8a308d10f8409bd009b064456
ksimran004 <sk04@uw.edu>
2023-08-09 15:50:41 -0700

    final submission

--- chart1_example.R
+++ chart1_example.R
@@ -1,4 +1,4 @@
-library(ggplot)
+library(ggplot2)
 library(dplyr)
 
 Country <- c("United States", "Afghanistan")
@@ -6,7 +6,7 @@ Prevalence <- c(3.098053, 4.424521)
 
 data <- data.frame(Country = Country, Prevalence = Prevalence)
 
-chart1 <- ggplot2(data, aes(x = Country, y = Prevalence, fill = Country, colour = Country )) +
+chart1 <- ggplot(data, aes(x = Country, y = Prevalence, fill = Country, colour = Country )) +
   geom_bar(stat = "identity", position = "dodge") +
   labs(x = "Country", y = "Prevalence of Depression (%)") +
   ggtitle("Comparison of Depression of the two Countries by age group 20-24 years") +
